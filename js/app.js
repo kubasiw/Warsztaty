749ab091fdd3e04d30fccb53e8219ad5cbe32330
Jakub Siwiński <kubsiw@gmail.com>
2016-11-08 18:10:46 +0100

    dodano slider z fotelami w sekcji podnawigacyjnej

--- js/app.js
+++ js/app.js
@@ -44,7 +44,56 @@ oFirmie.addEventListener('mouseout', function(hide){
     menu.style.display= "none";
 })
     
+// SLIDER
+
+var next = document.querySelector('.arrow_r');
+var prev = document.querySelector('.arrow_l');
+var slider = document.querySelector('.black_chair ul');
+var sliderElements = document.querySelectorAll('.black_chair ul li');
     
+
+//Ustawienie szerokości wszystkich slidów
+slider.style.width=(sliderElements.length+1)*470+"px";
+
+//ustawienie początkowe, aby nie było na pierwszym, bo by szarpało animacje itp.
+slider.style.left ="0px";
+document.querySelector('.black_chair ul li:nth-of-type(1)').classList.add('visible');
+    
+next.addEventListener('click',function(event){
+        
+//Przełożenie pierwszego elementu na koniec listy zdjęć
+var sliderWraper = document.querySelector('.black_chair ul');
+var firstElement = document.querySelector('.black_chair ul li:first-of-type');
+sliderWraper.appendChild(firstElement);
+
+//Usunięcie ze wszystkich li klasy visible 
+Array.from(document.querySelectorAll('.black_chair ul li')).forEach(function(element){
+   element.classList.remove('visible');
+});
+
+//Dodanie do aktualnego klasę visible
+document.querySelector('.black_chair ul li:nth-of-type(1)').classList.add('visible');
+                  
+});
+    
+prev.addEventListener('click',function(event){
+    
+//Przełożenie pierwszego elementu na koniec listy zdjęć
+var sliderWraper = document.querySelector('.black_chair ul');
+var firstElement =  document.querySelector('.black_chair ul li:first-of-type');
+var lastElement =  document.querySelector('.black_chair ul li:last-of-type');
+
+sliderWraper.insertBefore(lastElement,firstElement);
+
+//Usunięcie ze wszystkich li klasy visible 
+Array.from(document.querySelectorAll('.black_chair ul li')).forEach(function(element){
+    element.classList.remove('visible');
+});
+
+ //Dodanie do aktualnego klasę visible
+ document.querySelector('.black_chair ul li:nth-of-type(1)').classList.add('visible');
+         
+});
     
     
    
